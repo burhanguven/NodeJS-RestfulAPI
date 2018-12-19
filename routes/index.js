302035c57bc3663b2de0c5113804fd6646241826
Burhan Güven <burhanguven34@hotmail.com>
2018-12-19 23:10:47 +0300

    user bilgileri bcrypt ile şifrelendi

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,30 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt=require('bcryptjs');
 
-/* GET home page. */
-router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express' });
+const User=require('../models/User');
+
+//password db ye şifreli basmak için
+//bcrypt dependencies olarak eklemek için
+//npm install --save bcryptjs 
+router.post('/register', (req,res,next)=>{
+	const {username, password}=req.body;
+
+	bcrypt.hash(password,10).then((hash)=>{
+		const user= new User({
+			username,
+			password: hash
+		});
+
+		const promise=user.save();
+		promise.then((data)=>{
+			res.json(data);
+		}).catch((err)=>{
+			res.json(err);
+		})
+	})
 });
+
+
 
 module.exports = router;
